c2e7b67f36936207dba313d54892eaf5b6709b5d
Epiczhul <76625941+Epiczhul@users.noreply.github.com>
2024-10-06 09:24:24 +0000

    update

--- src/drivers/graphics/vmware_svga.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void vmware_set_resolution(uint16_t w, uint16_t h);
-void vmware_clear_screen(uint32_t color);
-void vmware_draw_pixel(int x, int y, uint32_t color);
-void graphics_install_vmware(uint16_t w, uint16_t h);
